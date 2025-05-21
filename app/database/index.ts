42be6194732a20c334610ec498830d50e419697e
Lucas Nathan J. da Silva <lucasnathanj@gmail.com>
2025-05-21 20:44:48 -0300

    refactor: create prisma folder and move prisma.schema into it; create deploy command

--- app/database/index.ts
+++ app/database/index.ts
@@ -1,10 +1,10 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "prisma/generated/client";
 import { Observer } from "./observer";
 
 const client = new PrismaClient();
 export const observer = new Observer(client);
-
 export const prisma = observer.prisma;
-export const {
-  // hello: Hello
+
+export const { 
+  user: User
 } = prisma;
