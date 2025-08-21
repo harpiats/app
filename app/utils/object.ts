b19b73935447947f2c98887d1f0e3c7c653d9317
Lucas Nathan J. da Silva <lucasnathanj@gmail.com>
2025-08-21 10:52:00 -0300

    refactor: improve type safety in ObjectUtility's omit methods

--- app/utils/object.ts
+++ app/utils/object.ts
@@ -32,14 +32,14 @@ export class ObjectUtility {
   /**
    * Omits specific properties from an object.
    */
-  public omit<T extends object>(obj: T, keys: string[]): Partial<T> {
-    return _.omit(obj, keys) as Partial<T>;
+  public omit<T extends object, K extends keyof T>(obj: T, keys: K[]): Omit<T, K> {
+    return _.omit(obj, keys) as Omit<T, K>;
   }
 
   /**
    * Omits specific properties from each object in a list.
    */
-  public omitFromList<T extends object>(list: T[], keys: string[]): Partial<T>[] {
+  public omitFromList<T extends object, K extends keyof T>(list: T[], keys: K[]): Omit<T, K>[] {
     return list.map((obj) => this.omit(obj, keys));
   }
 
